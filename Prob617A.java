5c7b8795c979b723e5bc3340f6c5c6b880ca3607
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-05-25 23:06:34 +0530

    Create Prob617A.java

---
+++ Prob617A.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Prob617A{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int step=sc.nextInt();
+		step=step+4;
+		step=step/5;
+		System.out.println(step);
+	}
+}
