dcee62b272981a46b2b7dcc4a3705c99c0332e79
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-05-25 14:07:22 +0530

    Create 50A.java

---
+++ 50A.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Prob50A{
+    public static void main(String[]args){
+        Scanner sc=new Scanner();
+        int m=sc.nextInt(),n=sc.nextInt();
+        int a=m*n;
+         a=a/2;
+         System.out.println(a);
+        
+    }
+}
