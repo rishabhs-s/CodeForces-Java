4a979ba49733c67b04cf4d119312bfb320ec2e06
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-05-28 19:19:01 +0530

    Create 110A.java

---
+++ 110A.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Prob110A{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int count=0;
+        long n=sc.nextLong();
+        while(n>0)
+        {
+        if(n%10==4 || n%10==7)
+        {
+            count++;
+        }
+        n=n/10;
+        }
+        if(count==4||count==7)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
